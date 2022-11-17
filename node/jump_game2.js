75d93ee51cbb90d2af3542727605c50027d91121
samick.hsu <samick.hsu@gmail.com>
2022-11-17 23:25:02 +0800

    1. Updated answer of jump_game

--- node/jump_game2.js
+++ node/jump_game2.js
@@ -5,11 +5,13 @@
 
 var canJump = function(nums) {
     let n = nums.length;
-    let last = n-1;
-    for(let i = n-2; i >= 0; i--){
-        if(i+nums[i] >= last) last = i;
+    let last = n - 1;
+    for(let i = n - 2; i >= 0; i--) {
+        if(i + nums[i] >= last) {
+            last = i;
+        }
     }
-    return last == 0;
+    return last === 0;
 };
 
 console.log(canJump([2, 3, 1, 1, 4]));
